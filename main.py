70bf154c3524d0d6d29e84ccb3664fa28354abc1
THChen <terryhorse2002@gmail.com>
2023-05-01 00:20:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import openai
 
+CHANNEL_ACCESS_TOKEN = ''
+CHANNEL_SECRET = ''
 
 messages = [{"role": "system", "content": "You are a helpful assistant"}]
 app = Flask(__name__)
@@ -18,10 +20,8 @@ def linebot():
     body = request.get_data(as_text=True)                    # 取得收到的訊息內容
     try:
         json_data = json.loads(body)                         # json 格式化訊息內容
-        access_token = ''
-        secret = ''
-        line_bot_api = LineBotApi(access_token)              # 確認 token 是否正確
-        handler = WebhookHandler(secret)                     # 確認 secret 是否正確
+        line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)              # 確認 token 是否正確
+        handler = WebhookHandler(CHANNEL_SECRET)                     # 確認 secret 是否正確
         signature = request.headers['X-Line-Signature']      # 加入回傳的 headers
         handler.handle(body, signature)                      # 綁定訊息回傳的相關資訊
         tk = json_data['events'][0]['replyToken']            # 取得回傳訊息的 Token
@@ -36,7 +36,70 @@ def linebot():
         line_bot_api.reply_message(tk,TextSendMessage(reply))# 回傳訊息
     except:
         print(body)                                          # 如果發生錯誤，印出收到的內容
-    return 'OK'                                              # 驗證 Webhook 使用，不能省略
+    return 'OK'       
+
+def send_remind_msg(text):
+    url = 'https://api.line.me/v2/bot/message/push'
+    headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer ' + CHANNEL_ACCESS_TOKEN,
+    }
+    payload = {
+        'to': group_id,
+        'messages': format_text_message(text),
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+
+def sendLineFlexMessage(text):
+    message = {
+        "type": "flex",
+        "altText": (current_date.month + 1) + "/" + current_date.day + "請假名單",
+        "contents": {
+            "type": "bubble",
+            "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": (current_date.month + 1) + "/" + current_date.day + "請假名單",
+                        "weight": "bold",
+                        "size": "xl"
+                    },
+                    {
+                        "type": "box",
+                        "layout": "vertical",
+                        "margin": "lg",
+                        "spacing": "sm",
+                        "contents": [
+                            {
+                                "type": "box",
+                                "layout": "horizontal",
+                                "spacing": "sm",
+                                "contents": [
+                                    {
+                                        "type": "text",
+                                        "size": "sm",
+                                        "flex": 1,
+                                        "text": text,
+                                        "wrap": True
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+    payload = {
+        "to": group_id, # 请填写你要发送的用户 ID
+        "messages": [message]
+    }
+    headers = {
+        "Authorization": "Bearer " + CHANNEL_ACCESS_TOKEN
+    }
+    response = requests.post("https://api.line.me/v2/bot/message/push", headers=headers, data=json.dumps(payload))                                       # 驗證 Webhook 使用，不能省略
 
 
 if __name__ == "__main__":
